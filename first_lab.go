69da7a69bc594ab6004d2c0ad22c8d889aad9180
Kolya Akulich <6adfeniks@gmail.com>
2021-11-29 23:10:44 +0300

    wow

--- first_lab.go
+++ first_lab.go
@@ -2,26 +2,25 @@ package main
 
 import "fmt"
 
-func factorial( x int) int{
-	if x == 0{
+func factorial(x int) int {
+	if x == 0 {
 		return 1
 	}
 	res := 1
-	for i:=1; i<=x; i++{
+	for i := 1; i <= x; i++ {
 		res *= i
 	}
 	return res
 
 }
 
-func main(){
+func main() {
 	var x int
 	fmt.Print("Enter the x: ")
 	fmt.Scanln(&x)
-	fac := factorial(x)
-	fmt.Printf("The factorial of %d is %d", x, fac)
+	var fac = factorial(x)
+	fmt.Printf("The factorial of %d is %d\n", x, fac)
 
 	fmt.Println("Something else?..")
 
 }
-

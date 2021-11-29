14fc753b5d07d984634c947a3701e49f14a7a362
Kolya Akulich <6adfeniks@gmail.com>
2021-11-29 22:22:57 +0300

    Add first code

---
+++ first_lab.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func factorial( x int) int{
+	if x == 0{
+		return 1
+	}
+	res := 1
+	for i:=1; i<=x; i++{
+		res *= i
+	}
+	return res
+
+}
+
+func main(){
+	var x int
+	fmt.Print("Enter the x: ")
+	fmt.Scanln(&x)
+	fac := factorial(x)
+	fmt.Printf("The factorial of %d is %d", x, fac)
+
+}
+

e466236fa5bf1ce7cd31183fcac838e46f6d5905
Kolya Akulich <6adfeniks@gmail.com>
2021-12-14 00:44:11 +0300

    .

--- project_date/main.go
+++ project_date/main.go
@@ -1,16 +1,11 @@
 /*Обьекты дат с методами добавления дней/месяцев/годов
 и другими методами
-
 */
 
 package main
 
-import "fmt"
-
 func main() {
-	var ss int
 	dates := []date{}
 	start(dates)
-	fmt.Scanln(&ss)
 
 }
